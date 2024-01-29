1b9012de358f7fce73d867631e449257b8c87cde
Ewen Le Bihan <hey@ewen.works>
2024-01-29 11:26:46 +0100

    🔨 Add LSP to dev deps, lint with rubocop

--- Gemfile
+++ Gemfile
@@ -3,5 +3,7 @@ source "https://rubygems.org"
 
 gemspec
 
+gem "omnibus", "~> 8.3", group: :development
+gem "ruby-lsp", group: :development
+gem "rufo", group: :development
 gem "solargraph", group: :development
-gem "omnibus", "~> 8.3", :group => :development
--- lib/fsorg.rb
+++ lib/fsorg.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "colorize"
 require "set"
 require "shellwords"
@@ -7,17 +9,17 @@ PERMISSIONS_PATTERN = /[ugoa]*([-+=]([rwxXst]*|[ugo]))+|[-+=]?[0-7]+/
 
 def d(thing)
   p thing
-  return thing
+  thing
 end
 
-class Fsorg
+class Fsorg # rubocop:disable Metrics/ClassLength,Style/Documentation
   attr_accessor :data, :document, :document_path, :root_directory
 
   def initialize(root_directory, data, document, absolute_path)
     @root_directory = root_directory
     @shell = "/usr/bin/env bash"
     @document_path = absolute_path
-    @data = { :HERE => @root_directory.to_s }.merge data
+    @data = { HERE: @root_directory.to_s }.merge data
     @document = document
     @current_line = 0
     @current_depth = -1
@@ -29,10 +31,10 @@ class Fsorg
 
   def depth_change(line)
     change = 0
-    if /\}$/ =~ line
+    if /\}\s*$/ =~ line
       change -= 1
     end
-    if /^\{/ =~ line
+    if /^\s*\{/ =~ line
       change += 1
     end
     change
@@ -43,7 +45,7 @@ class Fsorg
     strip_comments
     desugar
     process_includes
-    # TODO process_see
+    # TODO: process_see
     process_for
     process_if
     turn_puts_into_runs
@@ -52,13 +54,14 @@ class Fsorg
     process_shell
   end
 
-  def process_front_matter
+  def process_front_matter # rubocop:disable Metrics/MethodLength
     unless /^-+$/ =~ @document.lines(chomp: true).first
       return
     end
 
     # Remove front matter from document
-    front_matter_raw, rest = "", ""
+    front_matter_raw = ""
+    rest = ""
     inside_front_matter = false
     @document.lines(chomp: true).each_with_index do |line, index|
       @current_linecur = index + 1
@@ -68,19 +71,19 @@ class Fsorg
       end
 
       if inside_front_matter
-        front_matter_raw += line + "\n"
+        front_matter_raw += "#{line}\n"
       else
-        rest += line + "\n"
+        rest += "#{line}\n"
       end
     end
 
-    front_matter = YAML.load(front_matter_raw, symbolize_names: true) or {}
+    front_matter = YAML.safe_load(front_matter_raw, symbolize_names: true) or {}
 
     @data = front_matter.merge @data
     @document = rest
   end
 
-  def desugar
+  def desugar # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength,Metrics/PerceivedComplexity
     output = []
     inside_write_directive_shorthand = false
     @document.lines(chomp: true).each_with_index do |line, index|
@@ -101,8 +104,8 @@ class Fsorg
         output << $~[1]
         output << $~[2]
         output << "}"
-      elsif /^(?<filename>.+?)\s*(\s+\((?<permissions>#{PERMISSIONS_PATTERN.to_s})\))?\s*\[$/.match line.strip
-        output << "WRITE #{$~[:filename]} " + ($~[:permissions] ? "MODE #{$~[:permissions]}" : "") + " {"
+      elsif /^(?<filename>.+?)\s*(\s+\((?<permissions>#{PERMISSIONS_PATTERN})\))?\s*\[$/.match line.strip
+        output << "WRITE #{$~[:filename]} #{$~[:permissions] ? "MODE #{$~[:permissions]}" : ""} {"
         inside_write_directive_shorthand = true
       else
         output << line
@@ -113,23 +116,23 @@ class Fsorg
 
   def strip_comments
     output = []
-    @document.lines(chomp: true).each_with_index do |line, index|
-      if /^(?<content>.*)#\s.*$/ =~ line
-        output << content
+    @document.lines(chomp: true).each_with_index do |line, _index|
+      output << if /^(?<content>.*)#\s.*$/ =~ line
+        content
       else
-        output << line
+        line
       end
     end
     @document = output.join "\n"
   end
 
-  def process_includes
+  def process_includes # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
     output = []
 
     @document.lines(chomp: true).each_with_index do |line, index|
       @current_line = index + 1
       if line.start_with? "INCLUDE "
-        filepath = @document_path.parent.join(line.sub /^INCLUDE /, "")
+        filepath = @document_path.parent.join(line.sub(/^INCLUDE /, ""))
         included_raw = File.new(filepath).read.strip
         included_fsorg = Fsorg.new(@root_directory, @data, included_raw, filepath)
         included_fsorg.preprocess
@@ -149,25 +152,25 @@ class Fsorg
     @document = output.join "\n"
   end
 
-  def turn_puts_into_runs
+  def turn_puts_into_runs # rubocop:disable Metrics/MethodLength
     output = []
     @document.lines(chomp: true).each_with_index do |line, index|
       @current_line = index + 1
-      if /^PUT\s+(?<source>.+?)(\s+AS\s+(?<destination>.+?))?(\s+MODE\s+(?<permissions>.+?))?$/.match(line.strip)
-        output << "RUN install -D \"$DOCUMENT_DIR/#{$~[:source]}\" #{($~[:destination] || $~[:source]).shellescape}" + (if $~[:permissions]
+      output << if /^PUT\s+(?<source>.+?)(\s+AS\s+(?<destination>.+?))?(\s+MODE\s+(?<permissions>.+?))?$/.match(line.strip) # rubocop:disable Lint/MixedRegexpCaptureTypes
+        "RUN install -D \"$DOCUMENT_DIR/#{$~[:source]}\" #{($~[:destination] || $~[:source]).shellescape}" + (if $~[:permissions]
           " -m #{$~[:permissions].shellescape}"
         else
           ""
         end)
       else
-        output << line
+        line
       end
     end
 
     @document = output.join "\n"
   end
 
-  def process_for
+  def process_for # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
     output = []
     inside_for_directive = false
     body = []
@@ -177,7 +180,7 @@ class Fsorg
       @current_line = index + 1
       if inside_for_directive
         current_depth += depth_change line.strip
-        if current_depth == 0
+        if current_depth.zero?
           inside_for_directive = false
           output += repeat_for_each(args[:iteratee], args[:iterator], body)
         else
@@ -195,14 +198,11 @@ class Fsorg
     end
   end
 
-  def repeat_for_each(iteratee, iterator, directives)
+  def repeat_for_each(iteratee, iterator, directives) # rubocop:disable Metrics/AbcSize
     output = []
-    unless data[iterator.to_sym]
-      raise "[#{@document_path}:#{@current_line}]".colorize :red + "Variable '#{iterator}' not found (iterators cannot be asked for interactively). Available variables at this point: #{data.keys.join(", ")}."
-    end
-    unless data[iterator.to_sym].is_a? Array
-      raise "[#{@document_path}:#{@current_line}]".colorize :red + "Cannot iterate over '#{iterator}', which is of type #{data[iterator.to_sym].class}."
-    end
+    raise "[#{@document_path}:#{@current_line}]".colorize :red + "Variable '#{iterator}' not found (iterators cannot be asked for interactively). Available variables at this point: #{data.keys.join(", ")}." unless data[iterator.to_sym]
+    raise "[#{@document_path}:#{@current_line}]".colorize :red + "Cannot iterate over '#{iterator}', which is of type #{data[iterator.to_sym].class}." unless data[iterator.to_sym].is_a? Array
+
     data[iterator.to_sym].each do |item|
       output += directives.map do |directive|
         directive.gsub "{{#{iteratee}}}", item.to_s
@@ -211,7 +211,7 @@ class Fsorg
     output
   end
 
-  def process_if
+  def process_if # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength,Metrics/PerceivedComplexity
     output = []
     inside_if = false
     body_if = []
@@ -220,11 +220,11 @@ class Fsorg
     current_condition = nil
     current_depth = 0
 
-    @document.lines(chomp: true).each_with_index do |line, index|
+    @document.lines(chomp: true).each_with_index do |line, index| # rubocop:disable Metrics/BlockLength
       @current_line = index + 1
       if inside_if
         current_depth += depth_change line.strip
-        if current_depth == 0
+        if current_depth.zero?
           inside_if = false
           inside_else = false
           output += body_if if evaluates_to_true? current_condition
@@ -233,7 +233,7 @@ class Fsorg
         end
       elsif inside_else
         current_depth += depth_change line.strip
-        if current_depth == 0
+        if current_depth.zero?
           inside_else = false
           inside_if = false
           output += body_else if evaluates_to_false? current_condition
@@ -246,9 +246,8 @@ class Fsorg
         current_depth = 1
         inside_if = true
       elsif /^(\}\s*)?ELSE\s*\{$/.match(line.strip)
-        if current_condition.nil?
-          raise "[#{@document_path}:#{@current_line}] Cannot use ELSE without IF."
-        end
+        raise "[#{@document_path}:#{@current_line}] Cannot use ELSE without IF." if current_condition.nil?
+
         inside_else = true
         current_depth = 1
       else
@@ -263,29 +262,25 @@ class Fsorg
   end
 
   def evaluates_to_true?(condition)
-    unless @data.include? condition.to_sym
-      raise "[#{@document_path}:#{@current_line}] Variable '#{condition}' not found. Available variables at this point: #{data.keys.join(", ")}."
-    end
+    raise "[#{@document_path}:#{@current_line}] Variable '#{condition}' not found. Available variables at this point: #{data.keys.join(", ")}." unless @data.include? condition.to_sym
 
     @data[condition.to_sym]
   end
 
   def ask_missing_variables
-    @document.scan /\{\{(?<variable>[^}]+?)\}\}/ do |variable|
-      unless @data.include? variable[0].to_sym
-        @data[variable[0].to_sym] = :ask
-      end
+    @document.scan(/\{\{(?<variable>[^}]+?)\}\}/) do |variable|
+      @data[variable[0].to_sym] = :ask unless @data.include? variable[0].to_sym
     end
 
     @data.each do |key, value|
       if value == :ask
         print "#{key}? "
-        @data[key] = YAML.load STDIN.gets.chomp
+        @data[key] = YAML.safe_load $stdin.gets.chomp
       end
     end
   end
 
-  def process_root
+  def process_root # rubocop:disable Metrics/MethodLength
     @document = @document.lines(chomp: true).map.with_index do |line, index|
       @current_line = index + 1
       if /^ROOT\s+(?<root>.+?)$/.match line.strip
@@ -317,18 +312,16 @@ class Fsorg
     @data[:HERE]
   end
 
-  def walk(dry_run, quiet, verbose)
+  def walk(dry_run, quiet, verbose) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength,Metrics/PerceivedComplexity
     current_path = [@root_directory]
     @data[:HERE] = @root_directory
     file_to_write = {}
     inside_write_directive = -> { !file_to_write.keys.empty? }
-    current_path_as_pathname = -> { current_path.reduce(Pathname.new "") { |path, fragment| path.join fragment } }
+    current_path_as_pathname = -> { current_path.reduce(Pathname.new("")) { |path, fragment| path.join fragment } }
 
-    if verbose
-      puts "Data is #{@data.except :HERE}".light_black
-    end
+    puts "Data is #{@data.except :HERE}".light_black if verbose
 
-    @document.lines(chomp: true).each_with_index do |line, index|
+    @document.lines(chomp: true).each_with_index do |line, index| # rubocop:disable Metrics/BlockLength
       @current_line = index + 1
       @current_depth = current_path.length - 1
 
@@ -336,29 +329,25 @@ class Fsorg
         line = line.gsub "{{#{key}}}", value.to_s
       end
 
-      if inside_write_directive.()
+      if inside_write_directive.call
         if line.strip == "}"
           do_write file_to_write, dry_run, quiet
           file_to_write = {}
         else
-          file_to_write[:content] += line + "\n"
+          file_to_write[:content] += "#{line}\n"
         end
-      elsif /^WRITE(\s+INTO)?\s+(?<destination>.+?)(?:\s+MODE\s+(?<permissions>.+?))?\s*\{$/.match line.strip
+      elsif /^WRITE(\s+INTO)?\s+(?<destination>.+?)(?:\s+MODE\s+(?<permissions>.+?))?\s*\{$/.match line.strip # rubocop:disable Lint/MixedRegexpCaptureTypes
         file_to_write = $~.named_captures.transform_keys(&:to_sym)
         file_to_write[:content] = ""
       elsif /^(?<leaf>.+?)\s+\{/ =~ line.strip
         current_path << leaf
-        @data[:HERE] = current_path_as_pathname.()
-        if verbose
-          puts "currenly #{current_path.map { |fragment| fragment.to_s }.join " -> "}".light_black
-        end
+        @data[:HERE] = current_path_as_pathname.call
+        puts "currenly #{current_path.map(&:to_s).join " -> "}".light_black if verbose
         do_mkpath current_location, dry_run, quiet
       elsif line.strip == "}"
         current_path.pop
-        @data[:HERE] = current_path_as_pathname.()
-        if verbose
-          puts "currenly #{current_path.map { |fragment| fragment.to_s }.join " -> "}".light_black
-        end
+        @data[:HERE] = current_path_as_pathname.call
+        puts "currenly #{current_path.map(&:to_s).join " -> "}".light_black if verbose
       elsif /^RUN\s+(?<command>.+?)$/ =~ line.strip
         environment = {
           "FSORG_ROOT" => @root_directory.to_s,
@@ -372,58 +361,54 @@ class Fsorg
   end
 
   def do_mkpath(path, dry_run, quiet)
-    unless quiet
-      puts "#{"  " * @current_depth}+ ".cyan.bold + path.relative_path_from(@root_directory).to_s
-    end
-    unless dry_run
-      path.mkpath
-    end
+    puts "#{"  " * @current_depth}+ ".cyan.bold + path.relative_path_from(@root_directory).to_s unless quiet
+    path.mkpath unless dry_run
   end
 
-  def do_write(future_file, dry_run, quiet)
+  def do_write(future_file, dry_run, quiet) # rubocop:disable Metrics/AbcSize
     indentation = "  " * @current_depth
     dest = from_relative_to_root(current_location) / future_file[:destination]
-    unless dest.parent.relative_path_from(@root_directory).to_s == "."
-      do_mkpath dest.parent, dry_run, quiet
-    end
+    do_mkpath dest.parent, dry_run, quiet unless dest.parent.relative_path_from(@root_directory).to_s == "."
 
     unless quiet
       puts "#{indentation}> ".cyan.bold + dest.relative_path_from(@root_directory).to_s + (future_file[:permissions] ? " mode #{future_file[:permissions]}".yellow : "")
     end
-    unless dry_run
-      dest.write ensure_final_newline deindent replace_data future_file[:content]
-      # Not using dest.chmod as the syntax for permissions is more than just integers,
-      # and matches in fact the exact syntax of chmod's argument, per the manpage, chmod(1) (line "Each MODE is of the form…")
-      `chmod #{future_file[:permissions]} #{dest}` if future_file[:permissions]
-    end
+
+    return if dry_run
+
+    dest.write ensure_final_newline deindent replace_data future_file[:content]
+    # Not using dest.chmod as the syntax for permissions is more than just integers,
+    # and matches in fact the exact syntax of chmod's argument, per the manpage, chmod(1) (line "Each MODE is of the form…")
+    `chmod #{future_file[:permissions]} #{dest}` if future_file[:permissions]
   end
 
   def replace_data(content)
-    content.gsub /\{\{(?<variable>[^}]+?)\}\}/ do |interpolation|
+    content.gsub(/\{\{(?<variable>[^}]+?)\}\}/) do |interpolation|
       variable = interpolation[2..-3]
       @data[variable.to_sym]
     end
   end
 
-  def do_run(command, inside, environment, dry_run, quiet, verbose)
+  def do_run(command, inside, environment, dry_run, quiet, verbose) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Metrics/ParameterLists
     indentation = "  " * @current_depth
     unless quiet
       puts "#{indentation}$ ".cyan.bold + command + (verbose ? " at #{inside.relative_path_from(@root_directory)}".light_blue + " with ".light_black + (format_environment_hash environment) : "")
     end
-    unless dry_run
-      stdout, stdout_w = IO.pipe
-      stderr, stderr_w = IO.pipe
 
-      system environment, command, { :chdir => inside.to_s, :out => stdout_w, :err => stderr_w }
-      stdout_w.close
-      stderr_w.close
+    return if dry_run
 
-      stdout.read.each_line(chomp: true) do |line|
-        puts "  " + indentation + line
-      end
-      stderr.read.each_line(chomp: true) do |line|
-        puts "  " + indentation + line.red
-      end
+    stdout, stdout_w = IO.pipe
+    stderr, stderr_w = IO.pipe
+
+    system environment, command, { chdir: inside.to_s, out: stdout_w, err: stderr_w }
+    stdout_w.close
+    stderr_w.close
+
+    stdout.read.each_line(chomp: true) do |line|
+      puts "  #{indentation}#{line}"
+    end
+    stderr.read.each_line(chomp: true) do |line|
+      puts "  #{indentation}#{line.red}"
     end
   end
 
@@ -434,14 +419,12 @@ class Fsorg
   end
 end
 
-def deindent(text)
+def deindent(text) # rubocop:disable Metrics/MethodLength
   using_tabs = text.lines(chomp: true).any? { |line| /^\t/ =~ line }
   indenting_with = using_tabs ? "\t" : " "
   depth = text.lines.map do |line|
     count = 0
-    until line[count] != indenting_with
-      count += 1
-    end
+    count += 1 until line[count] != indenting_with
     count
   end.min
   pattern = /^#{using_tabs ? '\t' : " "}{#{depth}}/
@@ -452,10 +435,10 @@ def deindent(text)
 end
 
 def ensure_final_newline(text)
-  unless text.end_with? "\n"
-    text + "\n"
-  else
+  if text.end_with? "\n"
     text
+  else
+    "#{text}\n"
   end
 end
 
@@ -465,7 +448,7 @@ def capture_output
   $stdout = StringIO.new
   $stderr = StringIO.new
   yield
-  return $stdout.string, $stderr.string
+  [$stdout.string, $stderr.string]
 ensure
   $stdout = old_stdout
   $stderr = old_stderr
